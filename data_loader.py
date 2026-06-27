22bdde1ed5fa28f3d30ed51b15ebf828f99ca381
sujayconstructionc <sujayconstruction.c@gmail.com>
2026-06-27 14:20:02 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -5,24 +5,16 @@ import streamlit as st
 @st.cache_data(ttl=86400)
 def get_symbols(exchange):
 
-    try:
+    url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
 
-        url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
+    df = pd.read_csv(url)
 
-        df = pd.read_csv(url)
+    symbols = (
+        df["SYMBOL"]
+        .dropna()
+        .astype(str)
+        .unique()
+        .tolist()
+    )
 
-        symbols = (
-            df["SYMBOL"]
-            .dropna()
-            .astype(str)
-            .unique()
-            .tolist()
-        )
-
-        return [f"{s}.NS" for s in symbols]
-
-    except Exception as e:
-
-        st.error(f"Symbol Load Error: {e}")
-
-        return []
+    return [f"{s}.NS" for s in symbols]
