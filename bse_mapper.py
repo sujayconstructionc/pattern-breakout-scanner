75c421db85e08e47270572ed68c3c0f7c493edb9
sujayconstructionc <sujayconstruction.c@gmail.com>
2026-06-29 15:51:55 +0530

    Update bse_mapper.py

--- bse_mapper.py
+++ bse_mapper.py
@@ -1,103 +1,14 @@
-import pandas as pd
-import streamlit as st
-
-
-@st.cache_data(ttl=86400)
-def get_bse_master():
-
-    try:
-
-        url = (
-            "https://developer.paytmmoney.com/"
-            "data/v1/scrips/bse_security_master.csv"
-        )
-
-        df = pd.read_csv(url)
-
-        return df
-
-    except Exception:
-
-        return pd.DataFrame()
-
-
 def get_bse_yahoo_symbols():
 
-    df = get_bse_master()
-
-    if len(df) == 0:
-        return []
-
-    # Debug
-    print(df.columns.tolist())
-
-    code_col = None
-
-    preferred_cols = [
-        "Scrip Code",
-        "SCRIP_CODE",
-        "SC_CODE",
-        "BSECODE",
-        "BSE_CODE"
+    return [
+        "500325.BO",  # Reliance
+        "500209.BO",  # Infosys
+        "532540.BO",  # TCS
+        "500180.BO",  # HDFC Bank
+        "532174.BO",  # ICICI Bank
+        "500112.BO",  # SBI
+        "532215.BO",  # Axis Bank
+        "500696.BO",  # HUL
+        "500875.BO",  # ITC
+        "532978.BO"   # Bajaj Finserv
     ]
-
-    for c in preferred_cols:
-
-        if c in df.columns:
-
-            code_col = c
-            break
-
-    # Fallback
-    if code_col is None:
-
-        numeric_cols = []
-
-        for c in df.columns:
-
-            try:
-
-                sample = (
-                    df[c]
-                    .dropna()
-                    .astype(str)
-                    .head(20)
-                )
-
-                if all(
-                    len(x) == 6
-                    and x.isdigit()
-                    for x in sample
-                ):
-                    numeric_cols.append(c)
-
-            except:
-                pass
-
-        if len(numeric_cols):
-
-            code_col = numeric_cols[0]
-
-    if code_col is None:
-        return []
-
-    symbols = []
-
-    for code in df[code_col].dropna():
-
-        try:
-
-            code = str(int(float(code)))
-
-            if len(code) == 6:
-
-                symbols.append(
-                    f"{code}.BO"
-                )
-
-        except:
-            pass
-
-    symbols = list(dict.fromkeys(symbols))
-
-    return symbols
