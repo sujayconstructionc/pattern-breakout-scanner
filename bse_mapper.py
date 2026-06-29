dcca9ad2403eead2dffb8bcb9b20f62f1ed0c90b
sujayconstructionc <sujayconstruction.c@gmail.com>
2026-06-29 16:04:46 +0530

    Update bse_mapper.py

--- bse_mapper.py
+++ bse_mapper.py
@@ -1,14 +1,10 @@
 def get_bse_yahoo_symbols():
 
-    return [
-        "500325.BO",  # Reliance
-        "500209.BO",  # Infosys
-        "532540.BO",  # TCS
-        "500180.BO",  # HDFC Bank
-        "532174.BO",  # ICICI Bank
-        "500112.BO",  # SBI
-        "532215.BO",  # Axis Bank
-        "500696.BO",  # HUL
-        "500875.BO",  # ITC
-        "532978.BO"   # Bajaj Finserv
-    ]
+    df = get_bse_master()
+
+    print(df.columns.tolist())
+
+    if len(df) == 0:
+        return []
+
+    ...
